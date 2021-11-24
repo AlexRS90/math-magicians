5284c4e23df26af319193e6111c95167c11b63ac
AlexRS90 <aramosbengy@gmail.com>
2021-11-23 21:25:45 -0600

    Fix linters

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
+// eslint-disable-next-line
 class App extends React.Component {
   render() {
-    return (<div className="App">
+    return (
+    <div className="App">
       <h1>Math Magicians</h1>
-      <Calculator />
+      <Calculator></Calculator>
     </div>
     );
   }
