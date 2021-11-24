4bbc5c15f3b756fb57ca01ba4750743be2213af7
AlexRS90 <aramosbengy@gmail.com>
2021-11-23 21:42:05 -0600

    Fix linters

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-      <h1>Math Magicians</h1>
-      <Calculator />
-    </div>
+        <h1>Math Magicians</h1>
+        <Calculator />
+      </div>
     );
   }
 }
