788379d239f5b0785606498dbffed5e3b571cc4a
AlexRS90 <aramosbengy@gmail.com>
2021-11-24 19:25:35 -0600

    Refactor function's name

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -12,10 +12,10 @@ class Calculator extends Component {
         operation: '',
       },
     };
-    this.sendData = this.sendData.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  sendData = (obj, btnValue) => {
+  handleClick = (obj, btnValue) => {
     const newObj = calculate(obj, btnValue);
     if (!newObj.next && !newObj.total) {
       newObj.next = 0;
@@ -32,25 +32,25 @@ class Calculator extends Component {
           {obj.operation}
           {obj.next}
         </p>
-        <button onClick={() => this.sendData(obj, 'AC')} type="button">AC</button>
-        <button onClick={() => this.sendData(obj, '+/-')} type="button">+/-</button>
-        <button onClick={() => this.sendData(obj, '%')} type="button">%</button>
-        <button onClick={() => this.sendData(obj, '÷')} type="button" className="operations">&divide;</button>
-        <button onClick={() => this.sendData(obj, '7')} type="button" className="numbers">7</button>
-        <button onClick={() => this.sendData(obj, '8')} type="button" className="numbers">8</button>
-        <button onClick={() => this.sendData(obj, '9')} type="button" className="numbers">9</button>
-        <button onClick={() => this.sendData(obj, 'x')} type="button" className="operations">X</button>
-        <button onClick={() => this.sendData(obj, '4')} type="button" className="numbers">4</button>
-        <button onClick={() => this.sendData(obj, '5')} type="button" className="numbers">5</button>
-        <button onClick={() => this.sendData(obj, '6')} type="button" className="numbers">6</button>
-        <button onClick={() => this.sendData(obj, '-')} type="button" className="operations">-</button>
-        <button onClick={() => this.sendData(obj, '1')} type="button" className="numbers">1</button>
-        <button onClick={() => this.sendData(obj, '2')} type="button" className="numbers">2</button>
-        <button onClick={() => this.sendData(obj, '3')} type="button" className="numbers">3</button>
-        <button onClick={() => this.sendData(obj, '+')} type="button" className="operations">+</button>
-        <button onClick={() => this.sendData(obj, '0')} type="button" className="numbers zero">0</button>
-        <button onClick={() => this.sendData(obj, '.')} type="button" className="numbers">.</button>
-        <button onClick={() => this.sendData(obj, '=')} type="button" className="operations">=</button>
+        <button onClick={() => this.handleClick(obj, 'AC')} type="button">AC</button>
+        <button onClick={() => this.handleClick(obj, '+/-')} type="button">+/-</button>
+        <button onClick={() => this.handleClick(obj, '%')} type="button">%</button>
+        <button onClick={() => this.handleClick(obj, '÷')} type="button" className="operations">&divide;</button>
+        <button onClick={() => this.handleClick(obj, '7')} type="button" className="numbers">7</button>
+        <button onClick={() => this.handleClick(obj, '8')} type="button" className="numbers">8</button>
+        <button onClick={() => this.handleClick(obj, '9')} type="button" className="numbers">9</button>
+        <button onClick={() => this.handleClick(obj, 'x')} type="button" className="operations">X</button>
+        <button onClick={() => this.handleClick(obj, '4')} type="button" className="numbers">4</button>
+        <button onClick={() => this.handleClick(obj, '5')} type="button" className="numbers">5</button>
+        <button onClick={() => this.handleClick(obj, '6')} type="button" className="numbers">6</button>
+        <button onClick={() => this.handleClick(obj, '-')} type="button" className="operations">-</button>
+        <button onClick={() => this.handleClick(obj, '1')} type="button" className="numbers">1</button>
+        <button onClick={() => this.handleClick(obj, '2')} type="button" className="numbers">2</button>
+        <button onClick={() => this.handleClick(obj, '3')} type="button" className="numbers">3</button>
+        <button onClick={() => this.handleClick(obj, '+')} type="button" className="operations">+</button>
+        <button onClick={() => this.handleClick(obj, '0')} type="button" className="numbers zero">0</button>
+        <button onClick={() => this.handleClick(obj, '.')} type="button" className="numbers">.</button>
+        <button onClick={() => this.handleClick(obj, '=')} type="button" className="operations">=</button>
       </div>
     );
   }
