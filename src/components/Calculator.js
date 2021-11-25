ee9d2e0f1dbce28a04a1784bae899c5906c55bc8
AlexRS90 <aramosbengy@gmail.com>
2021-11-24 18:40:35 -0600

    Delete file

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,30 +1,56 @@
 import React, { Component } from 'react';
+import calculate from '../logic/calculate';
 
 // eslint-disable-next-line
 class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      obj: {
+        total: '0',
+        next: '',
+        operation: '',
+      },
+    };
+    this.sendData = this.sendData.bind(this);
+  }
+
+  sendData = (obj, btnValue) => {
+    const newObj = calculate(obj, btnValue);
+    if (!newObj.next && !newObj.total) {
+      newObj.next = 0;
+    }
+    this.setState({obj: newObj});
+  };
+
   render() {
+    const {obj} = this.state;
     return (
       <div className="calculator-container">
-        <p className="display-result">0</p>
-        <button type="button">AC</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button type="button" className="operations">&divide;</button>
-        <button type="button" className="numbers">7</button>
-        <button type="button" className="numbers">8</button>
-        <button type="button" className="numbers">9</button>
-        <button type="button" className="operations">X</button>
-        <button type="button" className="numbers">4</button>
-        <button type="button" className="numbers">5</button>
-        <button type="button" className="numbers">6</button>
-        <button type="button" className="operations">-</button>
-        <button type="button" className="numbers">1</button>
-        <button type="button" className="numbers">2</button>
-        <button type="button" className="numbers">3</button>
-        <button type="button" className="operations">+</button>
-        <button type="button" className="numbers zero">0</button>
-        <button type="button" className="numbers">.</button>
-        <button type="button" className="operations">=</button>
+        <p className="display-result">
+            {obj.total}
+            {obj.operation}
+            {obj.next}
+        </p>
+        <button onClick={() => this.sendData(obj, 'AC')} type="button">AC</button>
+        <button onClick={() => this.sendData(obj, '+/-')} type="button">+/-</button>
+        <button onClick={() => this.sendData(obj, '%')} type="button">%</button>
+        <button onClick={() => this.sendData(obj, '÷')} type="button" className="operations">&divide;</button>
+        <button onClick={() => this.sendData(obj, '7')} type="button" className="numbers">7</button>
+        <button onClick={() => this.sendData(obj, '8')} type="button" className="numbers">8</button>
+        <button onClick={() => this.sendData(obj, '9')} type="button" className="numbers">9</button>
+        <button onClick={() => this.sendData(obj, 'x')} type="button" className="operations">X</button>
+        <button onClick={() => this.sendData(obj, '4')} type="button" className="numbers">4</button>
+        <button onClick={() => this.sendData(obj, '5')} type="button" className="numbers">5</button>
+        <button onClick={() => this.sendData(obj, '6')} type="button" className="numbers">6</button>
+        <button onClick={() => this.sendData(obj, '-')} type="button" className="operations">-</button>
+        <button onClick={() => this.sendData(obj, '1')} type="button" className="numbers">1</button>
+        <button onClick={() => this.sendData(obj, '2')} type="button" className="numbers">2</button>
+        <button onClick={() => this.sendData(obj, '3')} type="button" className="numbers">3</button>
+        <button onClick={() => this.sendData(obj, '+')} type="button" className="operations">+</button>
+        <button onClick={() => this.sendData(obj, '0')} type="button" className="numbers zero">0</button>
+        <button onClick={() => this.sendData(obj, '.')} type="button" className="numbers">.</button>
+        <button onClick={() => this.sendData(obj, '=')} type="button" className="operations">=</button>
       </div>
     );
   }
