0f33f0ee6782688661bc75e73e695a9cea5292cf
AlexRS90 <aramosbengy@gmail.com>
2021-12-01 11:01:37 -0600

    Give style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import styles from './Navbar.module.css';
 
-const Navbar = ({title}) => {
+const Navbar = ({ title }) => {
   return (
-    <nav className="navbar  bg-primary">
-    <h1>
-      {title}
-    </h1>
-    <ul>
-      <li>
-        <Link to='/'>Home</Link>
-      </li>
-      <li>
-        <Link to='/Calculator'>Calculator</Link>
-      </li>
-      <li>
-        <Link to='/Quote'>Quote</Link>
-      </li>
-    </ul>
+    <nav className={styles.navbar}>
+      <h2 className={styles.text}>
+        {title}
+      </h2>
+      <ul className={styles.ul}>
+        <li className={styles.text}>
+          <Link to='/'>Home</Link>
+        </li>
+        <li className={styles.text}>
+          <Link to='/Calculator'>Calculator</Link>
+        </li>
+        <li className={styles.text}>
+          <Link to='/Quote'>Quote</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
