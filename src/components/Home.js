fd590a8fa9fe0dd9ad96e91949d7b1ed6f628532
AlexRS90 <aramosbengy@gmail.com>
2021-12-01 11:01:08 -0600

    Add interesting math's facts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
     <div className="container">
-      <h1>Página de inicio</h1>
-      <p>
-        <Link to="/Calculator">Let's sum</Link>
+      <h2 className="home-title">Welcome to our page</h2>
+      <p className="home-introduction">
+      October 14th is celebrated as World Maths Day.
+      Let us know some interesting and amazing facts about Mathematics.
+      </p>
+      <p className="home-facts">
+      <span className="bullets">1.</span> Zero ( 0 ) is the only number which can not be represented by Roman numerals. <br />
+      <span className="bullets">2.</span> What comes after a million, billion and trillion? A quadrillion, quintillion,
+      sextillion, septillion, octillion, nonillion, decillion and undecillion<br />
+      <span className="bullets">3.</span> Plus (+) and Minus (-) sign symbols were used as early as 1489 A.D<br />
+      <span className="bullets">4.</span> 2 and 5 are the only primes that end in 2 or 5<br />
+      <span className="bullets">5.</span> An icosagon is a shape with 20 sides<br />
+      <span className="bullets">6.</span> Among all shapes with the same perimeter a circle has the largest area.<br />
+      <span className="bullets">7.</span> Abacus is considered the origin of the calculator<br />
+      <span className="bullets">8.</span> 40 when written "forty" is the only number with letters in alphabetical order, 
+      while "one" is the only one with letters in reverse order<br />
+      <span className="bullets">9.</span> 'FOUR' is the only number in the English language that is spelt with the same 
+      number of letters as the number itself<br />
+      <span className="bullets">10.</span> From 0 to 1,000, the letter "A" only appears in 1,000 ("one thousand")<br />
+      </p>
+      <p className="home-last">
+      Words such as formula, equation and calculation sounds boring for 
+      those who hate Maths as a subject, whereas it is fun for those who 
+      have keen interest towards solving equations/problems.
       </p>
     </div>
   );
