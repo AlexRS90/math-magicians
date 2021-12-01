9aec4425a830274fb0ccc0d17dfbeba070b1a48a
AlexRS90 <aramosbengy@gmail.com>
2021-12-01 11:00:28 -0600

    Add a wrapper and a text line

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -19,31 +19,34 @@ const Calculator = () => {
   };
 
   return (
-    <div className="calculator-container">
-      <p className="display-result">
-        {obj.total}
-        {obj.operation}
-        {obj.next}
-      </p>
-      <button onClick={() => handleClick(obj, 'AC')} type="button">AC</button>
-      <button onClick={() => handleClick(obj, '+/-')} type="button">+/-</button>
-      <button onClick={() => handleClick(obj, '%')} type="button">%</button>
-      <button onClick={() => handleClick(obj, '÷')} type="button" className="operations">&divide;</button>
-      <button onClick={() => handleClick(obj, '7')} type="button" className="numbers">7</button>
-      <button onClick={() => handleClick(obj, '8')} type="button" className="numbers">8</button>
-      <button onClick={() => handleClick(obj, '9')} type="button" className="numbers">9</button>
-      <button onClick={() => handleClick(obj, 'x')} type="button" className="operations">X</button>
-      <button onClick={() => handleClick(obj, '4')} type="button" className="numbers">4</button>
-      <button onClick={() => handleClick(obj, '5')} type="button" className="numbers">5</button>
-      <button onClick={() => handleClick(obj, '6')} type="button" className="numbers">6</button>
-      <button onClick={() => handleClick(obj, '-')} type="button" className="operations">-</button>
-      <button onClick={() => handleClick(obj, '1')} type="button" className="numbers">1</button>
-      <button onClick={() => handleClick(obj, '2')} type="button" className="numbers">2</button>
-      <button onClick={() => handleClick(obj, '3')} type="button" className="numbers">3</button>
-      <button onClick={() => handleClick(obj, '+')} type="button" className="operations">+</button>
-      <button onClick={() => handleClick(obj, '0')} type="button" className="numbers zero">0</button>
-      <button onClick={() => handleClick(obj, '.')} type="button" className="numbers">.</button>
-      <button onClick={() => handleClick(obj, '=')} type="button" className="operations">=</button>
+    <div className="calc-wrapper">
+      <p className="calc-text">Let's do some Math!</p>
+      <div className="calculator-container">
+        <p className="display-result">
+          {obj.total}
+          {obj.operation}
+          {obj.next}
+        </p>
+        <button onClick={() => handleClick(obj, 'AC')} type="button">AC</button>
+        <button onClick={() => handleClick(obj, '+/-')} type="button">+/-</button>
+        <button onClick={() => handleClick(obj, '%')} type="button">%</button>
+        <button onClick={() => handleClick(obj, '÷')} type="button" className="operations">&divide;</button>
+        <button onClick={() => handleClick(obj, '7')} type="button" className="numbers">7</button>
+        <button onClick={() => handleClick(obj, '8')} type="button" className="numbers">8</button>
+        <button onClick={() => handleClick(obj, '9')} type="button" className="numbers">9</button>
+        <button onClick={() => handleClick(obj, 'x')} type="button" className="operations">X</button>
+        <button onClick={() => handleClick(obj, '4')} type="button" className="numbers">4</button>
+        <button onClick={() => handleClick(obj, '5')} type="button" className="numbers">5</button>
+        <button onClick={() => handleClick(obj, '6')} type="button" className="numbers">6</button>
+        <button onClick={() => handleClick(obj, '-')} type="button" className="operations">-</button>
+        <button onClick={() => handleClick(obj, '1')} type="button" className="numbers">1</button>
+        <button onClick={() => handleClick(obj, '2')} type="button" className="numbers">2</button>
+        <button onClick={() => handleClick(obj, '3')} type="button" className="numbers">3</button>
+        <button onClick={() => handleClick(obj, '+')} type="button" className="operations">+</button>
+        <button onClick={() => handleClick(obj, '0')} type="button" className="numbers zero">0</button>
+        <button onClick={() => handleClick(obj, '.')} type="button" className="numbers">.</button>
+        <button onClick={() => handleClick(obj, '=')} type="button" className="operations">=</button>
+      </div>
     </div>
   );
 };
